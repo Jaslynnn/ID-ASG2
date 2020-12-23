08e3b86ea0b5e49acf599842a480821c4472dcab
Jaslynnn <s10204654@connect.np.edu.sg>
2020-12-23 18:16:15 +0800

    home page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ID-ASG2
 Assignment 2
+
+Credits:
+https://dribbble.com/shots/8109236-Space-Whale
---
+++ script.js
@@ -0,0 +1,5 @@
+var button = document.getElementByclass('btn');
+button.onclick = function () {
+    document.getElementByclass('btn').scrollLeft += 100;
+};
+
