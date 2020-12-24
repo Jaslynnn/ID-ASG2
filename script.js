4cf559a657a87fb481b601817db7a0305aede0fd
Jaslynnn <s10204654@connect.np.edu.sg>
2020-12-24 15:36:15 +0800

    qwrewe

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var button = document.getElementByclass('btn');
-button.onclick = function () {
-    document.getElementByclass('btn').scrollLeft += 100;
-};
+let searchButton = document.querySelector("#search")
 
+//Add an event listener that runs the function sendApiRequest when clicked.
+
+searchButton.addEventListener("click", () =>{
+    console.log("button pressed")
+    sendApiRequest()
+})
+
+//An asynchronous function to fetch data from api.
+async function sendApiRequest(){
+    let APP_ID ="405a394c"
+    let API_KEY ="3104f9ddfb4ee96509c47474e498a8e7"
+    let response = await fetch (``);
+    console.log(response)
+}
+
+//function that does something with the data received from the API. Name customised to whatever you are doing with it
+function userAPIData(data){
+
+}
